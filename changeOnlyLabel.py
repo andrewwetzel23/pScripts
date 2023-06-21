8facdd8588840ab5e19fb08867d65e968339bc9f
awetzel@lionpowerusa.com <awetzel@lionpowerusa.com>
2023-06-21 14:40:24 -0400

    Updating for readibility and given description

--- changeOnlyLabel.py
+++ changeOnlyLabel.py
@@ -5,13 +5,18 @@ from tqdm import tqdm
 dir = browse_for_dir()
 texts = get_texts_from_dir(dir)
 
+"""
+Replaces the first charcter in each line of every text file in a chosen directory.
+
+"""
+
+newFirstCharacter = '2'
+
 for file in tqdm(texts):
-	with open(file, 'r') as f:
-	    lines = f.readlines()
+    with open(file, 'r') as f:
+        lines = f.readlines()
 
-	with open(file, 'w') as f:
-		for line in lines:
-			f.write('2')
-			f.write(line[1:])
-
-
+    with open(file, 'w') as f:
+        for line in lines:
+            f.write(newFirstCharacter)
+            f.write(line[1:])
