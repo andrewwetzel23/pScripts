a4aebb3541890d9b8ce4fc5d3418089266809d8d
awetzel@lionpowerusa.com <awetzel@lionpowerusa.com>
2023-08-30 15:10:31 -0400

    moved logger config to system

--- mf/defs.py
+++ mf/defs.py
@@ -1,16 +1,3 @@
-import logging
-
-logger = logging.getLogger('mf')
-
-def configure_logging(level=logging.DEBUG):
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    if logger.hasHandlers():
-        logger.handlers.clear()
-    logger.addHandler(handler)
-    logger.setLevel(level)
-
 
 IMAGE_EXTENSIONS = ['.png', '.jpg', '.jpeg', '.JPG', '.PNG', '.JPEG']
 VIDEO_EXTENSIONS = ['.mp4', '.avi', '.mov']
--- mf/system.py
+++ mf/system.py
@@ -1,4 +1,16 @@
 import subprocess
+import logging
+
+logger = logging.getLogger('mf')
+
+def ConfigureLogger(level=logging.DEBUG):
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    if logger.hasHandlers():
+        logger.handlers.clear()
+    logger.addHandler(handler)
+    logger.setLevel(level)
 
 # Runs a batch file
 def runBatchFile(batch_path):
