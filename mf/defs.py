8f8363d2df4f85b270b216b35e7d3c72d13c4b19
awetzel@lionpowerusa.com <awetzel@lionpowerusa.com>
2023-08-30 14:28:39 -0400

    Fixing logger bug

--- mf/defs.py
+++ mf/defs.py
@@ -1,11 +1,16 @@
 import logging
 
 logger = logging.getLogger('mf')
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
+
+def configure_logging(level=logging.DEBUG):
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    if logger.hasHandlers():
+        logger.handlers.clear()
+    logger.addHandler(handler)
+    logger.setLevel(level)
+
 
 IMAGE_EXTENSIONS = ['.png', '.jpg', '.jpeg', '.JPG', '.PNG', '.JPEG']
 VIDEO_EXTENSIONS = ['.mp4', '.avi', '.mov']
